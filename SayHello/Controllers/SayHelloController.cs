91b5a38387d66f6924e25c0b9fd52de44006d972
Yarem_ql39@student.itstep.org <Yarem_ql39@student.itstep.org>
2025-02-17 20:15:40 +0200

    Add project files.

---
+++ SayHello/Controllers/SayHelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SayHello.Controllers
+{
+	public class SayHelloController : ControllerBase
+	{
+		public void SayHello()
+		{
+            Console.WriteLine("Say hello");
+		}
+	}
+}
